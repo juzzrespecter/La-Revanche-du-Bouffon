8017d65b7212d9a00fdb009b7c29ed98a708cca2
Daniel Rodriguez <daniel.rodriguezpueyo@telefonica.com>
2026-03-12 16:22:21 +0100

    Primer estoque

---
+++ inquisitor/src/go.mod
@@ -0,0 +1,3 @@
+module inquisitor
+
+go 1.25.7
---
+++ inquisitor/src/spoof/Spoof.go
@@ -0,0 +1,16 @@
+import (
+	"bytes"
+)
+
+type ARP []byte
+func (hdr ARP)setHardwareType(type uint16) {
+
+}
+
+func (hdr ARP)setProtocolType() {
+
+}
+
+func Spoof(IP_src string, MAC_src string, IP_dst string, MAC_dst string) {
+
+}
