6d3498dc66e2073d785de3adfcae27172e911d57
Dani <dani@mrnovember>
2026-03-30 09:29:29 +0200

    asd

---
+++ stockholm/stockholm.go
@@ -0,0 +1,15 @@
+package stockholm
+
+import "flag"
+
+func init() {
+	flag.BoolVar()   //help
+	flag.BoolVar()   //version
+	flag.StringVar() //reverse
+	flag.BoolVar()   //silent
+
+}
+
+func main() {
+
+}
