86be70add342ed8d091dcdaaabec815f761213b2
Daniel Rodriguez <daniel.rodriguezpueyo@telefonica.com>
2026-04-03 12:05:49 +0200

    level3

--- reverse-me/level3/level3.c
+++ reverse-me/level3/level3.c
@@ -1,15 +1,35 @@
+#include <stdint.h>
+#include <stdbool.h>
+#include <string.h>
+#include <stdio.h>
+#include <stdlib.h>
+
+void ____syscall_malloc() {
+	puts("Good job.");
+}
+
 void ___syscall_malloc() {
 	puts("Nope.");
 	exit(1);
 }
 
 int main() {
-	char buffer[0x40];
-	int x;
+	uint64_t long0_50;
+	uint8_t int0_45;
+	char atoi_buffer[3]; //0x44, 0x43, 0x42
+	uint8_t int0_41; // 0x41
+	char buffer[23]; // 0x40
+	char memset_buffer[9]; //0x21
+	uint8_t int0_18; //0x18
+	uint8_t int0_c;
+	int int0_10; //0x10
+	int int0_8; //0x8
+
+	bool boolean_var; // nunca almacenada en stack
 
 	printf("Please enter key: ");
-	x = scanf("%23s", buffer);
-	if (x == 1) {
+	int0_8 = scanf("%23s", buffer);
+	if (int0_8 != 1) {
 		___syscall_malloc();
 	}
 	if (buffer[1] != '2') {
@@ -18,21 +38,57 @@ int main() {
 	if (buffer[0] != '4') {
 		___syscall_malloc();
 	}
-	fflush(/* Variable de _DYNAMIC section */);
-	memset(buffer + 31, 0, 9);
-	buffer[31] = '*';
-	char var_8 = 0; // rbp-0x41
-	*(long *)(buffer + 40) = 2; // rbp-0x18
-	*(int *)(buffer + 40) = 1; // rbp-0xc
-				   
-
-	if (strlen(buffer + 31) == 8) {
+	fflush(stdin);
+	memset(memset_buffer, 0, 9);
+	memset_buffer[0] = '*';
+	int0_41 = 0;
+	int0_18 = 2;
+	int0_c = 1;
+_main_167:
+	while(true) {	
+		int var1 = strlen(memset_buffer);
+		int0_45 = 0;
+		if (strlen(memset_buffer) < 8) {
+			long0_50 = int0_18;
+			int0_45 = strlen(buffer);
+			boolean_var = long0_50 < int0_45;
+		}
+_main_227:
+		if (boolean_var == 0) {
+			break ;
+		}
+		atoi_buffer[0] = buffer[int0_18];
+		atoi_buffer[1] = buffer[int0_18 + 1];
+		atoi_buffer[2] = buffer[int0_18 + 2];
+		memset_buffer[int0_c] = atoi(atoi_buffer);
+		int0_18 += 3;
+		int0_c += 1;
 	}
-	long var_64 = buffer[40];
-	
-	if (strlen(buffer) >= buffer[40]) {
+_main_321:
+	memset_buffer[int0_18] = 0;
+	printf("memset buffer: %s\n", memset_buffer);
+	int0_10 = strcmp(memset_buffer, "********");
+	if (int0_10 == -2)
+		___syscall_malloc();
+	if (int0_10 == -1)
+		___syscall_malloc();
+	if (int0_10 == 0) {
+		____syscall_malloc();
+		goto _main_599;
 	}
-
-
-
+	if (int0_10 == 1)
+		___syscall_malloc();
+	if (int0_10 == 2)
+		___syscall_malloc();
+	if (int0_10 == 3)
+		___syscall_malloc();
+	if (int0_10 == 4)
+		___syscall_malloc();
+	if (int0_10 == 5)
+		___syscall_malloc();
+	if (int0_10 == 0x73)
+		___syscall_malloc();
+	___syscall_malloc();
+_main_599:
+	return 0;
 }
