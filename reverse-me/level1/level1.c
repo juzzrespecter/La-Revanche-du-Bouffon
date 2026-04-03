89d4b7529dc338c3d9d4115a9e46fbe7d2fe962f
Daniel Rodriguez <daniel.rodriguezpueyo@telefonica.com>
2026-04-03 12:25:57 +0200

    level1

--- reverse-me/level1/level1.c
+++ reverse-me/level1/level1.c
@@ -1,18 +1,33 @@
 #include <stdio.h>
+#include <string.h>
 
 int main() {
-	char buffer[20]; // 0x80
+	char buffer[20]; // 0x80, carga en 0x7a
 	char input[108]; //0x6c
 
 	buffer[120] = 0;
-	*(int *)(buffer + 6) = "__st";
-	*(int *)(buffer + 10) = "ack_";
-	*(int *)(buffer + 14) = "chec";
-	buffer[16] = 'k';
-	buffer[17] = 0;
+	buffer[6] = '_';
+	buffer[7] = '_';
+	buffer[8] = 's';
+	buffer[9] = 't';
+	buffer[10] = 'a';
+	buffer[11] = 'c';
+	buffer[12] = 'k';
+	buffer[13] = '_';
+	buffer[14] = 'c';
+	buffer[15] = 'h';
+	buffer[16] = 'e';
+	buffer[17] = 'c';
+	buffer[18] = 'k';
+	buffer[19] = 0;
 
 	printf("Please enter key: ");
 	scanf("%s", input);
 
-
+	if (strcmp(input, buffer + 6) == 0) {
+		printf("Good job.\n");
+	} else {
+		printf("Nope.\n");
+	}
+	return 0;
 }
