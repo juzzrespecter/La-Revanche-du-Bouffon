18d345a8a2a8d6024dc5985e88d04159b48f1629
Daniel Rodriguez <daniel.rodriguezpueyo@telefonica.com>
2026-04-03 12:42:47 +0200

    done

--- reverse-me/level2/level2.c
+++ reverse-me/level2/level2.c
@@ -1,4 +1,7 @@
 #include <stdio.h>
+#include <stdbool.h>
+#include <string.h>
+#include <stdlib.h>
 
 void no() {
 	puts("Nope.");
@@ -6,12 +9,16 @@ void no() {
 }
 
 void ok() {
+	puts("Good job.");
 }
 
 int main() {
-	int bool_41;
+	int int0_48;
+	int bool_41; //0x41
 	char atoi_buffer[3]; //0x39 - 0x37
 	int int0_36; //0x36
+	int int0_14; //0x14 
+	int int0_10; //0x10 
 	char buffer[23]; // 0x35 - 0x1E
 	char buffer_2[9]; // 0x1d
 	
@@ -27,38 +34,39 @@ int main() {
 	if (buffer[0] != 0x30) {
 		no();
 	}
-	fflush();
+	fflush(stdin);
 	memset(buffer_2, 0, 9);
 	buffer_2[0] = 0x64;
-	int int0_36 = 0; //0x36 
-	int int0_14 = 2; //0x14 
-	int int0_10 = 1; //0x10 
-.main_221:
+	int0_36 = 0; 
+	int0_14 = 2; 
+	int0_10 = 1; 
+_main_221:
 	while (true) {
-		int x = strlen(buffer_2);
-		if (x < 0x8)
-			// jmp to main+286
-		// ebp - 0x48 = ebp - 0x14
-		int y = strlen(buffer);
-		if (y >= /*ebp - 0x48 */) {
+		bool_41 = 0;
+		int0_48 = int0_14;
+		if (strlen(buffer_2) < 0x8) {
+			bool_41 = (int0_48 < strlen(buffer));
+		}
+		if (bool_41 == 0) {
 			break ; // jmp main+378
 		}
-.main_302:
+_main_302:
 	//  ebp - 0x40
 	//  ebp - 0x14	
-		atoi_buffer[0] = buffer[c];
-		atoi_buffer[1] = buffer[c + 1];
-		atoi_buffer[2] = buffer[c + 2];
+		atoi_buffer[0] = buffer[int0_14];
+		atoi_buffer[1] = buffer[int0_14 + 1];
+		atoi_buffer[2] = buffer[int0_14 + 2];
 
-		buffer_2[d] = atoi(atoi_buffer);
+		buffer_2[int0_10] = atoi(atoi_buffer);
 		int0_14 = int0_14 + 3;
 		int0_10 = int0_10 + 1;
 	} // jmp main+221
-.main_378:
-	buffer[int0_10] = 0;
-	if (strcmp(buffer, "delabere") == 0) {
+_main_378:
+	buffer_2[int0_10] = 0;
+	if (strcmp(buffer_2, "delabere") == 0) {
 		ok();
+	} else {
+		no();
 	}
-	no();
 	return 0;
 }
